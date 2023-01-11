a676dd64726a3361fbb8526294b82a2edebbb6cc
Yuri Campos <yurinapoleao@gmail.com>
2023-01-11 08:34:55 -0300

    feat: Created structure of the routes private

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -6,13 +6,16 @@ const Menu = () => {
 			<nav>
 				<ul>
 					<li>
-						<Link to="/">Home</Link>
+						<Link to="/home">Home</Link>
 					</li>
 					<li>
 						<Link to="/customer-list">Customer List</Link>
 					</li>
 					<li>
 						<Link to="/customer-form">CustomerForm</Link>
+					</li>
+					<li>
+						<Link to="/">Log Out</Link>
 					</li>
 				</ul>
 			</nav>
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,13 @@ import Menu from "../components/Menu";
 import CustomerForm from "../pages/Customer/form";
 import CustomerList from "../pages/Customer/list";
 import Home from "../pages/Home";
-
 import LoginForm from "../pages/Login/form";
 
 const RoutesComponents = () => {
 	return (
 		<Routes>
-			<Route index element={<LoginForm />} />
-			<Route path="/" element={<Menu />}>
+			<Route path="/" element={<LoginForm />} />
+			<Route element={<Menu />}>
 				<Route path="home" element={<Home />} />
 				<Route path="customer-list" element={<CustomerList />} />
 				<Route path="customer-form" element={<CustomerForm />} />
