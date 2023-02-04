177365389f5c0966d69312d39c52e02e31c884b1
Yuri Campos <yurinapoleao@gmail.com>
2023-02-04 09:56:29 -0300

    feat: Consumer user API

--- src/pages/User/list/index.js
+++ src/pages/User/list/index.js
@@ -1,26 +1,25 @@
+import { useEffect } from "react";
 import Button from "../../../components/Button";
 import TableGrid from "../../../components/TableGrid";
 import { Container, Content, Row, Title } from "./styles";
 import utils from "./utils";
 
 const Form = () => {
-	const { columns, page, pageSize, setPage, setPageSize, navigate } = utils();
+	const {
+		user,
+		page,
+		columns,
+		loading,
+		pageSize,
+		setPage,
+		setPageSize,
+		navigate,
+		getUserList,
+	} = utils();
 
-	const rows = [
-		{ id: 1, name: "Snow", email: "Jon", status: true, admin: false },
-		{ id: 2, name: "Dale", email: "as", status: true, admin: false },
-		{ id: 3, name: "Nelson", email: "dadas", status: false, admin: false },
-		{ id: 4, name: "AOBA", email: "Joasdasn", status: false, admin: false },
-		{ id: 5, name: "Vamos", email: "aad", status: false, admin: false },
-		{ id: 6, name: "lá", email: "Jdddn", status: false, admin: false },
-		{ id: 7, name: "Chama", email: "ffddf", status: false, admin: true },
-		{ id: 8, name: "lá", email: "Jdddn", status: false, admin: false },
-		{ id: 9, name: "Chama", email: "ffddf", status: false, admin: true },
-		{ id: 10, name: "lá", email: "Jdddn", status: false, admin: false },
-		{ id: 11, name: "Chama", email: "ffddf", status: false, admin: true },
-		{ id: 12, name: "lá", email: "Jdddn", status: false, admin: false },
-		{ id: 13, name: "Chama", email: "ffddf", status: false, admin: true },
-	];
+	useEffect(() => {
+		getUserList();
+	}, []);
 
 	return (
 		<Container>
@@ -35,14 +34,16 @@ const Form = () => {
 				</Row>
 				<TableGrid
 					page={page}
+					loading={loading}
 					pageSize={pageSize}
 					columns={columns}
-					rows={rows}
+					rows={user || []}
 					rowHeight={52}
 					headerHeight={40}
 					rowsPerPageOptions={[5, 10, 20]}
 					onPageChange={(newPage) => setPage(newPage)}
 					onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+					onCellDoubleClick={({ id }) => navigate(String(id))}
 				/>
 			</Content>
 		</Container>
--- src/pages/User/list/utils.js
+++ src/pages/User/list/utils.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { getUsers } from "../../../services/user";
 import getColumnsWithActions from "../../../utils/interfaceFactory/getColumnsWithActions";
 import columnsInitial from "./columns";
 
 const utils = () => {
+	const [user, setUser] = useState([]);
+	const [loading, setLoading] = useState(false);
 	const [pageSize, setPageSize] = useState(5);
 	const [page, setPage] = useState(0);
 
@@ -15,6 +18,36 @@ const utils = () => {
 		},
 	});
 
-	return { columns, pageSize, page, setPage, setPageSize, navigate };
+	const getUserList = async () => {
+		setLoading(true);
+		try {
+			const { results } = await getUsers();
+			const rows = results
+				.map(({ status, admin, ...rest }) => ({
+					status: status ? "ATIVO" : "INATIVO",
+					admin: admin ? "VERDADEIRO" : "FALSO",
+					...rest,
+				}))
+				.sort((a, b) => a.id - b.id);
+
+			setUser(rows);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	return {
+		user,
+		page,
+		columns,
+		loading,
+		pageSize,
+		setPage,
+		setPageSize,
+		navigate,
+		getUserList,
+	};
 };
 export default utils;
