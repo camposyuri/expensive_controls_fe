da62fa4ce86b550bf9711a1575e36a25ea8a5460
Yuri Campos <yurinapoleao@gmail.com>
2023-02-14 08:12:02 -0300

    feat: Created user form

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 export const ButtonStyle = styled(MuiButton)`
 	width: ${({ width }) => width}px;
 	height: 45px;
+	color: ${({ color }) => color};
 `;
 
 ButtonStyle.displayName = "ButtonStyle";
